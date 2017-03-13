e8c49c80ba08b8761187f74b1e32411cd99aebd3
Alexander Kristian Armstrong <s154302@student.dtu.dk>
2017-03-13 10:13:53 +0100

    Test for DB2 implementation on index

--- IBMNetbank/WebContent/index.jsp
+++ IBMNetbank/WebContent/index.jsp
@@ -1,11 +1,12 @@
 <!DOCTYPE HTML><%@page language="java"
 	contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 <html>
+<%@ page import="classes.Database" %>
 <head>
 <title>index</title>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <body>
-
+<h1><% new Database(); %></h1>
 </body>
 </html>
--- IBMNetbank/src/classes/Database.java
+++ IBMNetbank/src/classes/Database.java
@@ -1,10 +1,44 @@
 package classes;
 
+import java.sql.*;
+
 public final class Database {
 	
-	public static User logIn() {
-		User user = new User();
-		return user;
+	static Connection con;
+	private final static String IP = "192.86.32.54";
+	private final static String PASSWORD = "FAGP2017";
+	private static Statement stmt;
+	private static ResultSet rs;
+	
+	public static void main(String[] args) {
+		System.out.println(logIn("Alexander", "Armstrong7"));
+	}
+	
+	private static void createConnection() {
+		try {
+			Class.forName("com.ibm.db2.jcc.DB2Driver");
+			con = DriverManager.getConnection(IP, "DTU19", PASSWORD);
+			con.setAutoCommit(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean logIn(String usr, String pass) {
+		createConnection();
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT password FROM User WHERE username = " + usr);
+			if(rs.getString(0).equals(pass)) {
+				con.close();
+				return true;
+			}
+			con.close();
+			return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 }
